6ad27ce3760a3e69a6670b8a644ee7a2f1563bac
Aluno_Programador3 <Aluno_Programador3@DT246206.pmsp.edu.local>
2025-09-01 15:55:21 -0300

    Exercicio incompleto encapsulamento

--- clases_03.py
+++ clases_03.py
@@ -1,23 +1,103 @@
-class Pessoa():
+class Pessoa:
+    def __init__(self, nome: str, idade: int, cpf: str, email: str, saldo: float, limite: float, conta_ativa: bool, telefone: str, endereco: str, senha: int):
+        self.nome = nome        
+        self.idade = idade    
+        self.__cpf = cpf                
+        self.__email = email            
+        self.__saldo = saldo              
+        self.__limite = limite            
+        self.__conta_ativa = conta_ativa  
+        self._telefone = telefone        
+        self._endereco = endereco        
+        self._senha = senha
     
-    def __init__(self, nome: str, estado_profissional: bool, idade: int, _contabancaria: float, _endereco: list, _senha_cartao: int, __email: str, __celular: int, _cpf: int, altura, _senha: str ):
-        self.nome = nome
-        self.estado_profisional = estado_profissional
-        self.idade = idade
-        self._contabacaria = _contabancaria
-        self._endereco = _endereco
-        self._senha_cartao = _senha_cartao
-        self.__email = __email
-        self.__celular = __celular
-        self._cpf = _cpf
-        self.altura = altura
-        self._senha = _senha
+    @property
+    def cpf(self):
+        return self.__cpf
+    
+    @cpf.setter
+    def cpf(self, valor):
+        self.__cpf = valor
 
-     
+    @property
+    def email(self):
+        return self.__email
+    
+    @email.setter
+    def email(self, valor):
+        self.__email = valor
+
+    @property
+    def saldo(self):
+        return self.__saldo
+    
+    @saldo.setter
+    def saldo(self, valor):
+        self.__saldo = valor
+
+    @property
+    def limite(self):
+        return self.__limite
+    
+    @limite.setter
+    def limite(self, valor):
+        self.__limite = valor
+
+    @property
+    def conta(self):
+        return self.__conta_ativa
+    
+    @conta.setter
+    def conta(self, valor):
+        self.__conta_ativa = valor
+
+    @property
+    def telefone(self):
+        return self._telefone
+    
+    @telefone.setter
+    def telefone(self, valor):
+        self._telefone = valor
+    
+    @property
+    def endereco(self):
+        return self._endereco
+    
+    @endereco.setter
+    def endereco(self, valor):
+        self._endereco = valor
+
+    @property
+    def senha(self):
+        return self._senha
+    
+    @senha.setter
+    def senha(self, valor):
+        self._senha = valor
+
+    def despositar(self, valor):
+        if valor > 0:
+            self.__saldo += valor
+            self.__log_transacao("Depósito", valor)
+            print(f"Deposito de {valor} realizado")
+            return True
+        else:
+            return False
+
+
+    
+
+
+    
+
+
+    
+
+
     
     
-    def CadastrandoEmail(self):
-        self.__email = input("Digite seu email")
+
+ 
 
      
 
