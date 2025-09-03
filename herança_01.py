afc8c1508b0ed90b06acc79ffebec45e8bea2ca2
Aluno_Programador3 <Aluno_Programador3@DT246206.pmsp.edu.local>
2025-09-03 15:49:50 -0300

    Conhecendo SGBD

--- herança_01.py
+++ herança_01.py
@@ -1,19 +1,43 @@
-class Personagem():
-    def __init__(self, nome: str, filme: bool, roupa: str, serie: bool, genero: str,  jogo: bool ):
-        self.roupa = roupa
-        self.serie = serie
+class Personagem:
+    def __init__(self, nome: str, idade: int, posicao: str, agilidade: int, forca: int, defesa: int, ger:int, lesao: bool, valor: float, clube: str, partidas: int):
         self.nome = nome
-        self.filme = filme
-        self.genero = genero
-        self.jogo = jogo
+        self.idade = idade
+        self.posicao = posicao
+        self.agilidade = agilidade
+        self.forca = forca
+        self.defesa = defesa
+        self.ger = ger
+        self.lesao = lesao
+        self.valor = valor
+        self.clube = clube
+        self.partidas = partidas
 
-    def Tema(self):
-        print(f"O personagem {self.nome} participa de um filme/serie de genero {self.genero}")
+    def jogar_partida(self):
+        if self.lesao == True:
+            return f"Jogador {self.nome} está machucado"
+        else:
+            return f"Jogador {self.nome} vem pra jogo"
+        
+    def jogador_estrela(self):
+        if self.ger >= 89:
+            return f"O {self.posicao} {self.nome}, é um dos melhores"
     
-    def Existe(self):
-        print(f"O personagem {self.nome} não existe na vida real")
+    def posse(self):
+        if self.idade < 28:
+            return f"O jovem {self.nome} com agilidade {self.agilidade} mantem a bola"
+        else:
+            return f"O experiente {self.nome} com força {self.forca} está com a posse"
+        
+    def trasnferencia(self):
+        if self.valor > 5000000:
+            return f"Este {self.posicao} é muito valioso"
+        elif self.valor > 1000000:
+            return f"O jogador {self.nome} custa caro"
+        else:
+            return f"{self.nome} tem um valor de transferencia normal"
+        
+    def time(self):
+        return f"O profissional {self.nome} joga no clube {self.clube}"
 
-    def Filme_Game(self):
-        print(f"{self.nome} é de um filme")
 
         
--- herança_02.py
+++ herança_02.py
@@ -1,34 +1,106 @@
 from herança_01 import Personagem
 
-class SuperHeroi(Personagem):
-    def __init__(self, nome, filme, roupa, serie, genero, poder):
-        super().__init__(nome)
-        self.poder = poder
+class Atacante(Personagem):
+    def __init__(self, nome, idade, posicao, agilidade, forca, defesa, ger, lesao, valor, clube, partidas, finalizacao: int):
+        super().__init__(nome, idade, posicao, agilidade, forca, defesa, ger, lesao, valor, clube, partidas)
+        self.finalizacao = finalizacao
     
-    def Tema(self):
-        print(f"O personagem {self.nome} participa de um filme/serie de genero de Ficção/Fantasia")
-    
-    def DescobrindoPoder(self):
-        print(f"{self.nome} tem poder de {self.poder}")
+    def fazer_gol(self):
+        if self.finalizacao > 80:
+            return f"O Atacante {self.nome} fez um golaço"
+        else:
+            return f"Marca um gol o atacante {self.nome}"
 
-class AntiHeroi(Personagem):
-    def __init__(self, nome, filme, roupa, serie, genero, rival):
-        super().__init__(nome)
-        self.rival = rival
-
-    def ArquiRival(self):
-        print(f"O Vilão {self.nome} tem como Rival o {self.rival}")
-
-class Pessoa(Personagem):
-    def __init__(self, nome, filme, ficiticio):
-        super().__init__(nome, filme)
-        self.ficticio = ficiticio
+class Meia(Personagem):
+    def __init__(self, nome, idade, posicao, agilidade, forca, defesa, ger, lesao, valor, clube, partidas, passe: int):
+        super().__init__(nome, idade, posicao, agilidade, forca, defesa, ger, lesao, valor, clube, partidas)
+        self.passe = passe
     
-    def Existe(self):
-        print(f"Personagem {self.nome} faz o filme {self.filme} mas ele existe na vida real")
+    def visao_de_jogo(self):
+        return f"O meia tem a visão de jogo aprimorada"
     
-class Franklin(Personagem):
+    def assistencia(self):
+        if self.passe > 85:
+            return f"{self.nome}, Meia, dá assistencias maravilhosas"
+        elif self.passe < 65:
+            return f"Apesar de jogar no meio de campo, ele não tem passes bons"
     
-    def Filme_Game(self):
-        print(f"{self.nome} na verdade é de um jogo")
+class Zagueiro(Personagem):
+    def __init__(self, nome, idade, posicao, agilidade, forca, defesa, ger, lesao, valor, clube, partidas):
+        super().__init__(nome, idade, posicao, agilidade, forca, defesa, ger, lesao, valor, clube, partidas)
+    
+    def estilo(self):
+        if self.defesa > self.forca:
+            return "Esse zagueiro tem mais tecnica"
+        elif self.defesa < self.forca:
+            return f"O zagueiro {self.nome} é mais forte"
+        else:
+            return f"{self.nome} é equilibrado"
+    
+    def marcar(self):
+        if self.defesa > 84 and self.forca > 80 and self.agilidade > 75:
+            return f"O {self.nome} é um dos melhores zagueiros atualmente"
+        else:
+            return f"É um bom zagueiro"
+        
+class Goleiro(Personagem):
+    def __init__(self, nome, idade, posicao, agilidade, forca, defesa, ger, lesao, valor, clube, partidas):
+        super().__init__(nome, idade, posicao, agilidade, forca, defesa, ger, lesao, valor, clube, partidas)
+    
+    def defender_penalti(self, player):
+        return f"O goleiro {self.nome} defendeu o penalti do jogador {player}"
 
+
+
+
+atacante1 = Atacante(
+    nome="Kaio Jorge", idade=25, posicao="Atacante", agilidade=87, forca=76,
+    defesa=40, ger=90, lesao=False, valor=7500000, clube="Flamengo",
+    partidas=120, finalizacao=92
+)
+
+
+meia1 = Meia(
+    nome="Matheus Pereira", idade=29, posicao="Meia", agilidade=82, forca=70,
+    defesa=60, ger=85, lesao=False, valor=4500000, clube="São Paulo",
+    partidas=210, passe=88
+)
+
+
+zagueiro1 = Zagueiro(
+    nome="Fabricio Bruno", idade=31, posicao="Zagueiro", agilidade=73, forca=85,
+    defesa=88, ger=86, lesao=False, valor=3500000, clube="Palmeiras",
+    partidas=180
+)
+
+
+goleiro1 = Goleiro(
+    nome="Cassio", idade=34, posicao="Goleiro", agilidade=80, forca=78,
+    defesa=90, ger=89, lesao=False, valor=2000000, clube="Corinthians",
+    partidas=250
+)
+
+
+
+print(atacante1.jogar_partida())
+print(atacante1.jogador_estrela())
+print(atacante1.fazer_gol())
+print(atacante1.time())
+print(atacante1.trasnferencia())
+
+print("------"*30)
+
+print(meia1.assistencia())
+print(meia1.visao_de_jogo())
+print(meia1.posse())
+print(meia1.trasnferencia())
+
+print("------"*30)
+
+print(zagueiro1.estilo())
+print(zagueiro1.marcar())
+print(zagueiro1.time())
+
+print("------"*30)
+
+print(goleiro1.defender_penalti("Yuri Alberto"))
